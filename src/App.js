d9d66d95201091dcb87c191dba4a313ea8f857c4
iamashutoshp <ap1853177@gmail.com>
2022-07-03 20:17:06 +0530

    structure/frontend : ready

--- src/App.js
+++ src/App.js
@@ -1,25 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Blog, Feature, Footer, Header, Possibility, WhatGTP3 } from './containers'
+import { CTA, Navbar, Brand } from './components'
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div>Gpt-3</div>
+  )
 }
-
-export default App;
---
+++ src/containers/whatGTP3/WhatGTP3.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './whatGTP3.css'
+
+export default function WhatGTP3() {
+  return (
+    <div>WhatGTP3</div>
+  )
+}
