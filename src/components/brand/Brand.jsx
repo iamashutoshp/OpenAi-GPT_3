86316a9966451db15192a012b1639960e9790619
iamashutoshp <ap1853177@gmail.com>
2022-07-05 23:00:23 +0530

    feat/frontend : Brands added

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
-import './brand.css'
+import React from "react";
+import { google, slack, dropbox, atlassian, shopify } from "../../assets";
+import "./brand.css";
 
 export default function Brand() {
   return (
-    <div>Brand</div>
-  )
+    <div className="gpt3__brand__container">
+      <div>
+        <img src={google} alt="google" />
+      </div>
+      <div>
+        <img src={slack} alt="slack" />
+      </div>
+      <div>
+        <img src={dropbox} alt="dropbox" />
+      </div>
+      <div>
+        <img src={atlassian} alt="atlassian" />
+      </div>
+      <div>
+        <img src={shopify} alt="shopify" />
+      </div>
+    </div>
+  );
 }
